02f5e193f67ec921eb5ffc74d2be73887799a045
nic-instruction <nicole.bade@seattlecolleges.edu>
2016-12-06 11:15:32 -0800

    removed sudo
    This will be run as root, so the sudo command would just generate errors.

--- app_install.py
+++ app_install.py
@@ -2,13 +2,13 @@ import os
 
 def install_apache():
    print('installing pache')
-   os.system('sudo yum -y install httpd')
+   os.system('yum -y install httpd')
   
    print('enabling apache server')
-   os.system('sudo systemctl enable httpd.service')
+   os.system('systemctl enable httpd.service')
 
    print('statring apache server')
-   os.system('sudo systemctl start httpd.service')
+   os.system('systemctl start httpd.service')
 
    print('please open a security setting for port 80 on your server')
  
