2ecf1b80cd28e8f5d71bdfc2571bbf1c8edde1d1
nic-instruction <nicole.bade@seattlecolleges.edu>
2016-12-08 16:49:34 -0800

    quick cleenup
    cleaned out some troubleshooting code I had commented out.

--- test_launch_pythonboot.py
+++ test_launch_pythonboot.py
@@ -10,18 +10,11 @@ client = boto3.client('ec2')
 amazon_image = 'ami-2051294a'                                       # This will launch a red hat instance
 amazon_instance = 't2.micro'                                        # we've been working with micro's, if you use Amizon Linux, you could launch a nono
 amazon_pem_key = 'load-ballancer-static-content'                    # the name of the key/pem file you would like to use to access this machine
-#firewall_profile = 'launch-wizard-2'                                # the security group name you would like to use, remember, this is your firewall, make sure the ports you want open are open
+#firewall_profile = 'launch-wizard-2'                               # the security group name you would like to use, remember, this is your firewall, make sure the ports you want open are open
 
 print(amazon_image)
 print(amazon_instance)
 print(amazon_pem_key)
-
-
-#UserData = """#!/user/bin/python
-#import sys
-#sys.stdout = open('/tmp/file', 'w')
-#print 'test'
-#"""
 
 def launch_test_instance():
 
@@ -56,8 +49,7 @@ app_install.install_apache()
     )
 
    pprint.pprint(instances)
-   #response = client.describe_instances(instances)
-   #pprint.pprint(response)
+
 
 launch_test_instance()
 
